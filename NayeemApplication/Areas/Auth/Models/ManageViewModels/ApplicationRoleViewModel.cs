14a72c7c254ea78f19041744dfd173fbe847351a
“Nayeem-Bhuiyangit config --global user.email “nayeembhuiyan40@gmail.com <58185521+Nayeem-Bhuiyan@users.noreply.github.com>
2022-06-20 17:21:16 +0600

    fghfghdf

--- NayeemApplication/Areas/Auth/Models/ManageViewModels/ApplicationRoleViewModel.cs
+++ NayeemApplication/Areas/Auth/Models/ManageViewModels/ApplicationRoleViewModel.cs
@@ -35,6 +35,8 @@ namespace NayeemApplication.Areas.Auth.Models.AccountViewModels
 
         public string aspnetId { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public bool? isActive { get; set; }
         public DateTime? DateOfBirth { get; set; }
